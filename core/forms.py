bf0a48258b9e918037c6e59572667633ae89ebee
jmrkfnz <119049235+jmarkfen@users.noreply.github.com>
2023-03-18 09:47:51 +0800

    Install additional dependencies
    asgiref==3.6.0
    crispy-bootstrap5==0.7
    Django==4.1.6
    django-bootstrap-datepicker-plus==5.0.3
    django-crispy-forms==2.0
    pydantic==1.10.6
    sqlparse==0.4.3
    typing_extensions==4.5.0
    tzdata==2022.7

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,7 @@
 from typing import Text
 from django import forms
+from django.utils.translation import gettext as _
+from crispy_forms.layout import Submit
 from crispy_forms.helper import FormHelper
 from .models import Block, Set
 
@@ -9,12 +11,58 @@ class SetForm(forms.ModelForm):
         model = Set
         fields = '__all__'
 
+
 class BlockForm(forms.ModelForm):
     
     class Meta:
         model = Block
         fields = '__all__'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-        self.helper = FormHelper(self)
+
+class EventForm(forms.Form):
+
+    text = forms.CharField(
+        max_length = 50, 
+        required = True,
+    )
+    day = forms.ChoiceField(
+        choices = (
+        ('Mon', _('Monday')),
+        ('Tue', _('Tuesday')),
+        ('Wed', _('Wednesday')),
+        ('Thu', _('Thursday')),
+        ('Fri', _('Friday')),
+        ('Sat', _('Saturday')),
+        ('Sun', _('Sunday')),
+        ),
+        required = True,
+    )
+    start_time = forms.TimeField(
+
+        required = True,
+    )
+    end_time = forms.TimeField(
+
+        required = True,
+    )
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper(self)
+    #     self.helper.form_id = 'id-exampleForm'
+    #     self.helper.form_class = 'blueForms'
+    #     self.helper.form_method = 'post'
+    #     self.helper.form_action = 'submit_survey'
+    #     self.helper.add_input(Submit('submit', 'Submit'))
+    #     self.helper.form_class = 'form-horizontal'
+    #     self.helper.label_class = 'col-lg-2'
+    #     self.helper.field_class = 'col-lg-8'
+    #     self.helper.layout = Layout(
+    #             'email',
+    #             'password',
+    #             'remember_me',
+    #             StrictButton('Sign in', css_class='btn-default'),
+    #     )
+
+
+
