dfc3a9a6deab4ec87fb4b9ab5b6d4f02f6afed7c
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 21:09:24 +0800

    add auth urls

--- iskedyul/urls.py
+++ iskedyul/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("django.contrib.auth.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
