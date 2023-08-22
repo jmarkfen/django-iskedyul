45ae0bab007578744e151c5e6f6f2a54c27f538e
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 18:59:13 +0800

    Update admin.py

--- timetables/admin.py
+++ timetables/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Timetable, Event
+from django.utils import timezone
+timezone.activate("Asia/Manila")
+
 
 # Register your models here.
+
+admin.site.register(Timetable)
+admin.site.register(Event)
