357720e508c281db3edb570f97f5723751635847
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 21:52:47 +0800

    Update views.py

--- timetables/views.py
+++ timetables/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse 
 from django.shortcuts import render
+from fbv.decorators import render_html
 
 # Create your views here.
+
+
+@render_html() # index.html
+def index(request):
+    return {}
