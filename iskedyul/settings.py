cb74ed3f3385eed71f7b175e829d8ca8406409ed
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 18:59:27 +0800

    set server time zone

--- iskedyul/settings.py
+++ iskedyul/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'django_fbv',
+    'timetables',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-ph'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
