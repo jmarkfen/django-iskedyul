db5664fd659ba6a776bc7aec4260ee7c9d9ead16
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 21:08:51 +0800

    install django reload

--- iskedyul/urls.py
+++ iskedyul/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('timetables.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
