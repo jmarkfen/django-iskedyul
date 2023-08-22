e2c33b33465b7886eaca2d7cbc1dd4b56f9c437d
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 21:52:41 +0800

    Update urls.py

--- timetables/urls.py
+++ timetables/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from . import views
+from fbv.decorators import render_html
 
 urlpatterns = [
-    
+    path('', views.index),
 ]
