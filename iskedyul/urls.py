00ed3f3c58cd56f85cfe4ba230c811eae942a18f
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 21:53:03 +0800

    add auth views

--- iskedyul/urls.py
+++ iskedyul/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("django.contrib.auth.urls")),
+
+    #
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='login'),
+
+    # timetables
     path('', include('timetables.urls')),
+
+    # django browser reload
     path("__reload__/", include("django_browser_reload.urls")),
 ]
