fd6785eed1b0b8f5c6461ebd4ed5f15ec9c123f4
jmarkfen <119049235+jmarkfen@users.noreply.github.com>
2023-08-22 19:13:12 +0800

    add fbv middleware

--- iskedyul/settings.py
+++ iskedyul/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'fbv.middleware.RequestMethodMiddleware',
 ]
 
 ROOT_URLCONF = 'iskedyul.urls'
